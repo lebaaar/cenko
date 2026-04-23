9800d177fa05a2f80009133f1a88f490c83d4f1e
Lan <lanlebar6@gmail.com>
2026-04-23 22:31:47 +0200

    remove unused method

--- lib/features/auth/data/user_repository.dart
+++ lib/features/auth/data/user_repository.dart
@@ -29,31 +29,6 @@ class UserRepository {
     await _users.doc(uid).update({'settings': settings.toMap()});
   }
 
-  Future<void> deleteUser(String uid) async {
-    final userDoc = _users.doc(uid);
-
-    final receipts = await userDoc.collection('receipts').get();
-    for (final receipt in receipts.docs) {
-      final items = await receipt.reference.collection('items').get();
-      for (final item in items.docs) {
-        await item.reference.delete();
-      }
-      await receipt.reference.delete();
-    }
-
-    final shoppingList = await userDoc.collection('shopping_list').get();
-    for (final doc in shoppingList.docs) {
-      await doc.reference.delete();
-    }
-
-    final commonProducts = await userDoc.collection('common_products').get();
-    for (final doc in commonProducts.docs) {
-      await doc.reference.delete();
-    }
-
-    await userDoc.delete();
-  }
-
   Future<bool> userExists(String uid) async {
     final doc = await _users.doc(uid).get();
     return doc.exists;
