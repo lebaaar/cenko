e4f7ca71dc7769be8a2d99f10083434e6308c5e5
Lan <lanlebar6@gmail.com>
2026-05-21 07:51:33 +0200

    update release build config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -63,6 +63,7 @@ android {
         release {
             // `flutter run --release`
             signingConfig = signingConfigs.getByName("release")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
